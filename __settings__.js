4839c0ec559cce1f576f728279a88e4efbdc337a
meowmeow10 <165931265+meowmeow10@users.noreply.github.com>
2024-07-05 09:42:31 +0000

    added XR

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2029962.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 187367310, 187367364, 187367362, 187367215, 187367313, 187367366, 187367354, 187367353, 187367312, 187367368, 187367365, 187367370, 187367372, 187367369, 187367314, 187367248, 187367371, 187367315, 187367359, 187367252, 187367358, 187367360 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
