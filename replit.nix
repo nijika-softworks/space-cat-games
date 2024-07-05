cea4afe8295de38280a3da852fab2ec1ef8e612e
meowmeow10 <165931265+meowmeow10@users.noreply.github.com>
2024-07-05 09:28:18 +0000

    Initial commit

---
+++ replit.nix
@@ -0,0 +1,20 @@
+{pkgs}: {
+  deps = [
+    pkgs.xorg.xkill
+    pkgs.nano
+    pkgs.neofetch
+    pkgs.xorg.xinit
+    pkgs.openssh
+    pkgs.x11vnc
+    pkgs.x11docker
+    pkgs.x11spice
+    pkgs.nim2Packages.x11
+    pkgs.nimPackages.x11
+    pkgs.yaralyzer
+    pkgs.python311Packages.m3u8
+    pkgs.musikcube
+    pkgs.mysql80
+    pkgs.nmap
+    pkgs.wget
+  ];
+}
